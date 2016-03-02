895efa02739f371f2d8117de8c87e08632fd3735
TungLe <lethanhtung2908@gmail.com>
2016-03-02 14:41:45 +0700

    MMR (version 1)

---
+++ src/mmr/main.py
@@ -0,0 +1,10 @@
+__author__ = 'MichaelLe'
+
+
+import numpy as np
+import vector as vec
+
+a = np.array([10,2,1])
+b = np.array([1,3,11])
+
+print(vec.cosine(a,b))
---
+++ src/mmr/vector.py
@@ -0,0 +1,31 @@
+__author__ = 'MichaelLe'
+
+from numpy import *
+from numpy import linalg as LA
+
+
+def converArr(s):
+    lenS = ceil(len(s)/2.0)
+    a = zeros((1,lenS ))
+    i=0
+    for c in s:
+        if (c != ' '):
+            a[0,i] = c
+            i = i+1
+    return a
+
+def dotProduct(a, b):
+    n = size(a,0)
+    sum = 0
+    for i in range(0,n):
+        sum = sum + a[i]*b[i]
+    return sum
+
+def cosine(a, b):
+    c =  dotProduct(a,b)
+    d =  linalg.norm(a)*linalg.norm(b)
+    return (c/d)
+
+def similarity(a,b):
+    re = cosine(a,b)
+    return re
